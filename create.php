36fc5a428b11b16a66f98fbc2494b8e0117eeadb
surya <n.suryap@gmail.com>
2013-07-04 17:48:08 +0530

    Added create

---
+++ create.php
@@ -0,0 +1,16 @@
+<?php
+	$order = $_POST['order'];
+	$f=$_POST['formdata'];
+	
+	$formdata=unserialize($f);
+	$form=array();
+	
+	foreach($formdata as $q=>$w){
+		for($e=1;$e<=$w;++$e){
+			array_push($form,)		
+		}
+	}
+	
+	
+
+?>
--- layout.php
+++ layout.php
@@ -1,4 +1,16 @@
 <?php	
+	/*
+	$name+number+l --> Label 
+	$name+number+n --> Name
+	$name+number+i --> Id
+	$name+number+v --> Value
+	Also Pass form from here .. done
+	Layout specifier Array ==> Select Field (1 -- n) is order[] .. done
+
+*/
+
+
+
 		$ele=$_POST['elements'];
 		
 		 // Check empty
@@ -9,7 +21,7 @@
 		
 		$num=$_POST['num'];
 		$form=array();
-		$i=0;
+		$i=0;$s=1;
 		
 		for($n=0;$n<4;++$n){
 			if(!($num[$n]<=0) ){
@@ -19,16 +31,34 @@
 		}
 		echo "<html><head><title>Layout</title></head>";
 		echo "<body>";
+		echo "<form method='post' action='create.php' />";
+		echo "<div id='dispall' >";
 		foreach($form as $e => $c){
 			for($x=0;$x<$c;++$x){
-				echo "<label>".$e."No.".($x+1)."<label/><input type='text' name='".$e.($x+1).'l'. "' placeholder = 'Label for this field ' />";	
+				echo "<label>".$s++.") ".$e."No.".($x+1)."<label/><input type='text' name='".$e.($x+1).'l'. "' placeholder = 'Label for this field ' />";	
 				echo "<input type='text' name='".$e.($x+1).'n'."' placeholder = 'Name of this field' />";
 				echo "<input type='text' name='".$e.($x+1).'i'."' placeholder = 'Id for this field' />";
 				echo "<input type='text' name='".$e.($x+1).'v'."' placeholder = 'Value of this field' /><br />";
 			}
 		echo "<br />";
 		}
+	echo "</div><div id='specify' >";
+	for($l=1;$l<$s;++$l){
+		echo "<label>".$l."</label> -> <select name='order[]' >";
+		for($v=1;$v<$s;++$v){
+			echo "<option value='".$v."' ";
+			if($v==$l) echo "selected";
+			echo " >".$v."</option>";		
+		}
+		echo "</select>&nbsp;";	
+		if($l % 3==0) echo "<br />";
+	}
 	
+	
+	echo "</div>";
+	echo "<input type='hidden' name= 'formdata' value=" . serialize($form) . "/>";
+	echo "<input type='submit' value='Create' name= 'create' />";
+	echo "</form>";
 		
 
 ?>
