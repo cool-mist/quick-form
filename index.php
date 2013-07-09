81933e69759cb4a81b8f0aab8a8cf647af956d3a
surya <n.suryap@gmail.com>
2013-07-09 19:06:33 +0530

    Added Like Button

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	<script type="text/javascript" src='jquery.js' ></script>
 	<script type="text/javascript" src="indexjs.js" ></script>
 	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-  	<img src='img/header.jpg' id='header'/><br />	
+  	<img src='img/header.jpg' id='header'/><br />
+  	<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fsuryaformcreator&amp;send=false&amp;layout=standard&amp;width=450&amp;show_faces=true&amp;font=trebuchet+ms&amp;colorscheme=dark&amp;action=like&amp;height=80" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:80px;" allowTransparency="true"></iframe>	
 	</head>
 	
 	<body>
